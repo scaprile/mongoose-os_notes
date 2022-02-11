71773be15b785e6c41a48c00e550f19fd16693c8
Sergio R. Caprile <scaprile@gmail.com>
2022-02-11 19:16:56 -0300

    Added 'mqtt'
    Signed-off-by: Sergio R. Caprile <scaprile@gmail.com>

---
+++ mqtt/README.md
@@ -0,0 +1,6 @@
+# Using MQTT with Mongoose-OS and the ESP32
+Mongoose-OS has a built-in MQTT client and a simple mJS interpreter (Cesanta's reduced JavaScript version). This allows us to quickly and easily connect to an MQTT broker and develop proof-of-concept testbeds for more complex applications.
+
+See my blog for further info.
+
+Más información en [mi blog](http://www.scaprile.com/2022/02/11/mqtt-con-esp32-y-mongoose-os/)
---
+++ mqtt/fs/init.js
@@ -0,0 +1,47 @@
+// Load Mongoose OS API
+load('api_sys.js');
+load('api_config.js');
+load('api_mqtt.js');
+load('api_log.js');
+
+/*
+ * get event values, lookup mongoose.h:
+ *
+ * #define MG_MQTT_CMD_CONNACK 2
+ * #define MG_MQTT_EVENT_BASE 200
+ *
+ * #define MG_EV_CLOSE 5
+ *
+ * #define MG_EV_MQTT_CONNACK (MG_MQTT_EVENT_BASE + MG_MQTT_CMD_CONNACK)
+ *
+*/
+let MG_EV_MQTT_CONNACK = 202;
+let MG_EV_CLOSE = 5;
+
+let device_id = Cfg.get('device.id');
+
+// MQTT publish
+let publish = function (topic, msg) {
+    let ok = MQTT.pub(topic, msg, 1, false);	// QoS = 1, do not retain
+    Log.print(Log.INFO, 'Published:' + (ok ? 'OK' : 'FAIL') + ' topic:' + topic + ' msg:' +  msg);
+    return ok;
+};
+
+// MQTT susbscribe
+MQTT.sub('/this/sub', function(conn, topic, msg) {
+    Log.print(Log.INFO, 'Got a msg: ' + msg);
+}, null);
+
+// MQTT startup
+MQTT.setEventHandler(function(conn, ev, edata) {
+    if (ev === MG_EV_MQTT_CONNACK) {
+        Log.print(Log.INFO, 'MQTT connected');
+	publish('/this/pub/'+device_id,'CONNECTED!');
+    }
+    else if (ev === MG_EV_CLOSE) {
+        Log.print(Log.ERROR, 'MQTT disconnected');
+    }
+}, null);
+
+Log.print(Log.WARN, "### init script started ###");
+
